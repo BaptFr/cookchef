c99753550bde96c9e77094ca47ffd4b76a9df3bb
BaptFr <baptistesalazar@gmail.com>
2024-12-17 17:53:59 +0100

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-<<<<<<< HEAD
-import './index.scss'
-=======
-import './assets/styles/index.scss'
->>>>>>> 1a430ade2824ca7a9e9dc5ecbb5bb9e3a2c57a70
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
