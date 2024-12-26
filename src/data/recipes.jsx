b032c70b6f03817797f0f38ce00d20ad0f9d3c98
BaptFr <baptistesalazar@gmail.com>
2024-12-26 17:20:39 +0100

    Like list state for futur basket change in wichlist

---
+++ src/data/recipes.jsx
@@ -0,0 +1,69 @@
+
+
+export const data =[
+    {
+        title: "Salade detox au poulet ",
+        _key: "9b992f3b-6519-499e-9a35-bea449da958c",
+        image:
+            "https://images.unsplash.com/photo-1505576633757-0ac1084af824?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "Pavés de saumon aux épinards ",
+        _key: "c16654d4-98f9-4a80-a5dd-bdc6dae28fe7",
+        image:
+            "https://images.unsplash.com/photo-1485921325833-c519f76c4927?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "Cookies chocolat / noix de pécan ",
+        _key: "732e4102-2bf4-4200-852f-39eefadd5062",
+        image:
+            "https://plus.unsplash.com/premium_photo-1670895802114-dc3bc13b5963?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "Velouté de butternut et graines de courges ",
+        _key: "329d6641-3a5e-41c5-b336-5afe0740a866",
+        image:
+            "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "Velouté de butternut et graines de courges ",
+        _key: "24f2891f-8731-48b1-bebf-677a458b6709",
+        image:
+            "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "Velouté de butternut et graines de courges ",
+        _key: "3c40c27f-b122-4bb5-b42d-1ef388298a4a",
+        image:
+            "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        title: "Velouté de butternut et graines de courges ",
+        _key: "9ad9579e-0f7f-4e9b-9e41-dd39c047223a",
+        image:
+            "https://unsplash.com/fr/photos/deux-sauces-garnies-de-graines-w6ftFbPCs9I",
+    },
+    {
+        title: "Velouté de butternut et graines de courges ",
+        _key: "3c91c951-fc9c-4720-9e79-266bce36a0f6",
+        image:
+            "https://unsplash.com/fr/photos/deux-sauces-garnies-de-graines-w6ftFbPCs9I",
+    },{
+        title: "Velouté de butternut et graines de courges ",
+        _key: "7c9a989f-2191-4b36-aed3-83049e0135e3",
+        image:
+            "https://unsplash.com/fr/photos/deux-sauces-garnies-de-graines-w6ftFbPCs9I",
+    },
+    {
+        title: "Velouté de butternut et graines de courges ",
+        _key: "fe415a5c-25f5-456f-be52-752a287000ac",
+        image:
+            "https://unsplash.com/fr/photos/deux-sauces-garnies-de-graines-w6ftFbPCs9I",
+    },
+    {
+        title: "Velouté de butternut et graines de courges ",
+        _key: "5630812f-3561-4b6f-a477-099f87d6f45d",
+        image:
+            "https://unsplash.com/fr/photos/deux-sauces-garnies-de-graines-w6ftFbPCs9I",
+    },
+]
